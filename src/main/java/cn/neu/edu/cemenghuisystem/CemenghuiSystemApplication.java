a61ca74b5f73e773bad5f52bae5e7d665a176f20
jasonsu <2981309541@qq.com>
2025-06-26 10:54:53 +0800

    first

---
+++ src/main/java/cn/neu/edu/cemenghuisystem/CemenghuiSystemApplication.java
@@ -0,0 +1,13 @@
+package cn.neu.edu.cemenghuisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemenghuiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CemenghuiSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/neu/edu/cemenghuisystem/CemenghuiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.neu.edu.cemenghuisystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CemenghuiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
