9e88c4e76a3e31ba6698e9d15928c7884c5d215a
FeliksZemdegsGit <feliks2005@qq.com>
2025-07-03 19:18:14 +0800

    vue基本结构创建完毕

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import './style.css'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus) // 确保这一行存在
+app.mount('#app')
