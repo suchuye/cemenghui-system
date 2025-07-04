a250e4b3a2fb267929921019641107252465c89d
Z784555 <20236682@stu.neu.edu.cn>
2025-07-04 23:10:19 +0800

    路由更改

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -8,26 +8,26 @@ const router = createRouter({
       //登录页面
       path: '/login',
       name: 'login',
-      component: () => import('../views/Login.vue'),
+      component: () => import('../loginSystem/Login.vue'),
     },
     //2、行业动态管理子系统
     {
         //动态详情页面
       path: '/news-detail',
       name: 'newsdetail',
-      component: () => import('../views/NewsDetail.vue'),
+      component: () => import('../industryNews/NewsDetail.vue'),
     },
     {
         //动态表单页面
       path: '/news-form',
       name: 'newsform',
-      component: () => import('../views/NewsForm.vue'),
+      component: () => import('../industryNews/NewsForm.vue'),
     },
     {
         //动态列表页面
       path: '/news-list',
       name: 'newslist',
-      component: () => import('../views/NewsList.vue'),
+      component: () => import('../industryNews/NewsList.vue'),
     },
   ],
 })
